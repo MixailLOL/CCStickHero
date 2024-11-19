28b7546233f0a2a7a432a60e2f367f414b4b169a
MixailLOL <medvedztel7@gmail.com>
2024-11-19 13:42:29 +0300

    player to start pos

--- StickHero/assets/Scripts/GameCtrl.ts
+++ StickHero/assets/Scripts/GameCtrl.ts
@@ -14,6 +14,7 @@ export class GameCtrl extends Component {
     public isRotated = true;
     public leftWallToPlayPos = false;
     public rightWallToPlayPos = false;
+    public playerToPlayPos = false;
     public activeWall : [leftWall: Node, rightWall: Node] = [];
     @property({
         type: Player,
@@ -53,19 +54,29 @@ export class GameCtrl extends Component {
         this.activeWall.rightWall = cc.instantiate(this.wallPref);
         this.activeWall.rightWall.parent = this.node.parent;
 
-        let wallWAdoptivWidthScale = (view.getVisibleSize().width)/(this.activeWall.leftWall.width*3.5);
-        let wallWAdoptivHeightScale = (view.getVisibleSize().height)/(this.activeWall.leftWall.height*5);
+        let wallAdoptivWidthScale = (view.getVisibleSize().width)/(this.activeWall.leftWall.width*3.5);
+        let wallAdoptivHeightScale = (view.getVisibleSize().height)/(this.activeWall.leftWall.height*5);
+
+        let playerAdoptivWidthScale = (view.getVisibleSize().width)/(this.player.node.width*13);
+        let playerAdoptivHeightScale = (view.getVisibleSize().height)/(this.player.node.height*13);
 
         let minRightWWS = (view.getVisibleSize().width)/(this.activeWall.leftWall.width*10);
         let maxRightWWS = (view.getVisibleSize().width)/(this.activeWall.leftWall.width*4);
 
-        this.activeWall.rightWall.setScale(math.randomRange(minRightWWS, maxRightWWS),wallWAdoptivHeightScale,0);
+        this.activeWall.rightWall.setScale(math.randomRange(minRightWWS, maxRightWWS),wallAdoptivHeightScale,0);
         this.activeWall.rightWall.setPosition(view.getVisibleSize().width,-view.getVisibleSize().height);
 
-        this.activeWall.leftWall.setScale(wallWAdoptivWidthScale,wallWAdoptivHeightScale,0);
-        this.activeWall.leftWall.setPosition(0,-(view.getVisibleSize().height/2+(this.activeWall.leftWall.height*wallWAdoptivHeightScale*(1/6))));
-        console.log(this.activeWall.leftWall.getPosition(), view.getVisibleSize())
-        this.player.initPos();;
+        this.activeWall.leftWall.setScale(wallAdoptivWidthScale,wallAdoptivHeightScale,0);
+        this.activeWall.leftWall.setPosition(0,-(view.getVisibleSize().height/2+(this.activeWall.leftWall.height*wallAdoptivHeightScale*(1/6))));
+        
+        
+
+        this.player.node.setScale(playerAdoptivWidthScale, playerAdoptivHeightScale);
+
+        let playerInitPosY = this.activeWall.leftWall.getPosition().y+this.activeWall.leftWall.width*this.activeWall.leftWall.getScale().y;
+        this.player.node.setPosition(0,playerInitPosY);
+        //this.player.node.setPosition(this.activeWall.leftWall.width*this.activeWall.leftWall.getScale().x/2-this.player.node.width*playerAdoptivWidthScale/2,0);
+
         this.userInt.initPos();
     }
 
@@ -109,12 +120,12 @@ export class GameCtrl extends Component {
             let positionX = -(view.getVisibleSize().width/2 - (this.activeWall.leftWall.width*this.activeWall.leftWall.getScale().x)/2);
             let positionY = -(view.getVisibleSize().height/2 - (this.activeWall.leftWall.height*this.activeWall.leftWall.getScale().y)/2);
             if(this.activeWall.leftWall.getPosition().x >= positionX ||  this.activeWall.leftWall.getPosition().y <= positionY){
-                if(this.activeWall.leftWall.getPosition().x >= positionX){
+                if(this.activeWall.leftWall.getPosition().x > positionX){
                     rigidBody.linearVelocity = new Vec2(-35, rigidBody.linearVelocity.y);     
                 }else{
                     rigidBody.linearVelocity = new Vec2(0, rigidBody.linearVelocity.y); 
                 }
-                if(this.activeWall.leftWall.getPosition().y <= positionY){
+                if(this.activeWall.leftWall.getPosition().y < positionY){
                     rigidBody.linearVelocity = new Vec2(rigidBody.linearVelocity.x,35);
                 }else{
                     rigidBody.linearVelocity = new Vec2(rigidBody.linearVelocity.x,0);
@@ -122,8 +133,22 @@ export class GameCtrl extends Component {
             }
             else{
                 this.leftWallToPlayPos = false;
+                this.playerToPlayPos = true;
                 rigidBody.linearVelocity = new Vec2(0, 0); 
             }  
+        }
+
+        if(this.playerToPlayPos){
+            let positionX = (this.activeWall.leftWall.getPosition().x + this.activeWall.leftWall.width*this.activeWall.leftWall.getScale().x/2-this.player.node.width*this.player.node.scale.x/2);
+            let rigidBody = this.player.node.getComponent(RigidBody2D);
+            if(this.player.node.getPosition().x < positionX ){
+                console.log(this.player.node.getPosition().x, positionX);
+                rigidBody.linearVelocity = new Vec2(4, 0);     
+            }
+            else{
+                this.playerToPlayPos = false;
+                rigidBody.linearVelocity = new Vec2(0, 0); 
+            } 
         }
 
         if(this.rightWallToPlayPos){
@@ -135,12 +160,12 @@ export class GameCtrl extends Component {
             //console.log(leftBorder, rightBorder, positionX)
             let positionY = -(view.getVisibleSize().height/2 - (this.activeWall.rightWall.height*this.activeWall.rightWall.getScale().y)/2);
             if(this.activeWall.rightWall.getPosition().x >= positionX ||  this.activeWall.rightWall.getPosition().y <= positionY){
-                if(this.activeWall.rightWall.getPosition().x >= positionX){
+                if(this.activeWall.rightWall.getPosition().x > positionX){
                     rigidBody.linearVelocity = new Vec2(-35, rigidBody.linearVelocity.y);     
                 }else{
                     rigidBody.linearVelocity = new Vec2(0, rigidBody.linearVelocity.y); 
                 }
-                if(this.activeWall.rightWall.getPosition().y <= positionY){
+                if(this.activeWall.rightWall.getPosition().y < positionY){
                     rigidBody.linearVelocity = new Vec2(rigidBody.linearVelocity.x,35);
                 }else{
                     rigidBody.linearVelocity = new Vec2(rigidBody.linearVelocity.x,0);
